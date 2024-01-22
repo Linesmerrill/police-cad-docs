5acae4c7a53a1d726c2b416ce2e4111a397dc520
Merrill Lines <linesmerrill@gmail.com>
2024-01-22 12:35:59 -0700

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,54 @@
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header
+      className={clsx("hero hero--primary", styles.heroBanner)}
+      style={{ backgroundImage: `url('/img/lpc_background.png')` }}
+    >
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg margin-right--sm"
+            to="/docs/intro"
+          >
+            Documentation
+          </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/api-specs"
+          >
+            API Specification
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
